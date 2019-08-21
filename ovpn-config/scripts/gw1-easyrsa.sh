4bafe8d3fc5565fbe4b5c444a59b77e4381afc7f
Damiano Giorgi <dgiorgi@facilitylive.com>
2019-08-21 12:40:11 +0200

    refactor

--- ovpn-config/scripts/gw1-easyrsa.sh
+++ ovpn-config/scripts/gw1-easyrsa.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 yum install -y easy-rsa
 
+# Copy default variables
+cp -f /vagrant/ovpn-config/scripts/easyrsa-vars  /usr/share/easy-rsa/3/vars
+
+# Build pki 
 cd /usr/share/easy-rsa/3/
 /usr/share/easy-rsa/3/easyrsa --batch init-pki
 /usr/share/easy-rsa/3/easyrsa --batch build-ca nopass
 /usr/share/easy-rsa/3/easyrsa --batch gen-dh nopass
 /usr/share/easy-rsa/3/easyrsa --batch build-server-full gw1 nopass
 /usr/share/easy-rsa/3/easyrsa --batch build-client-full gw2 nopass
+
+# Copy certs to openvpn dir
 mkdir -p /etc/openvpn/keys
-cp -f /vagrant/vars  /usr/share/easy-rsa/3/
 chmod +x /usr/share/easy-rsa/3/vars
 cp /usr/share/easy-rsa/3/pki/ca.crt  /etc/openvpn/keys/
 cp /usr/share/easy-rsa/3/pki/dh.pem  /etc/openvpn/keys/
 cp /usr/share/easy-rsa/3/pki/issued/gw1.crt /etc/openvpn/keys
 cp /usr/share/easy-rsa/3/pki/private/gw1.key /etc/openvpn/keys
+
+# Copy certs and keys to vagrant shared folder to provision gw2 openvpn 
 mkdir -p /vagrant/ovpn-config/certs/
 cp -f /usr/share/easy-rsa/3/pki/issued/gw2.crt /vagrant/ovpn-config/certs/
 cp -f /usr/share/easy-rsa/3/pki/private/gw2.key  /vagrant/ovpn-config/certs/
--- ovpn-config/scripts/gw2-easyrsa.sh
+++ ovpn-config/scripts/gw2-easyrsa.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 mkdir -p /etc/openvpn/keys
+
+# Copy gw1 generated certs from vagrant shared folder
 cp /vagrant/ovpn-config/certs/ca.crt  /etc/openvpn/keys/
 cp /vagrant/ovpn-config/certs/gw2.crt /etc/openvpn/keys
 cp /vagrant/ovpn-config/certs/gw2.key /etc/openvpn/keys
